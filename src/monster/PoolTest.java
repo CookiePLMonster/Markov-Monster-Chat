fe64ca9e0cc0184e94ea9257e1509807810ec067
Adrian Zdanowicz <zdanowia@len.iem.pw.edu.pl>
2015-06-14 22:16:50 +0200

    Test jednostkowy

---
+++ src/monster/PoolTest.java
@@ -0,0 +1,29 @@
+package monster;
+
+import junit.framework.TestCase;
+
+import java.util.List;
+
+public class PoolTest extends TestCase {
+    private Pool    pool;
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+        pool = new Pool();
+        pool.insertWord("slowoA");
+        pool.insertWord("slowoB");
+        pool.insertWord("slowoC");
+    }
+
+    public void testGetSize() throws Exception {
+        assertEquals(pool.getSize(), 3);
+    }
+
+    public void testGetList() throws Exception {
+        List<String> list = pool.getList();
+
+        assertEquals(list.get(2), "slowoC");
+        assertEquals(list.get(0), "slowoA");
+    }
+}
